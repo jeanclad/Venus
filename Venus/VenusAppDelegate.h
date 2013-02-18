8ba17255252ea8f8c2eee07d39f5cf2011804436
jeanclad <jaanclad@naver.com>
2013-02-18 18:11:40 +0900

    Added First Source Code

---
+++ Venus/VenusAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VenusAppDelegate.h
+//  Venus
+//
+//  Created by 권 회경 on 13. 2. 18..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenusAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Venus/VenusViewController.h
@@ -0,0 +1,13 @@
+//
+//  VenusViewController.h
+//  Venus
+//
+//  Created by 권 회경 on 13. 2. 18..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenusViewController : UIViewController
+
+@end
