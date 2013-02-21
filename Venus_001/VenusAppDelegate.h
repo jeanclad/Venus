2cbf492d3cb350d6aca02e6c2b45dd9229e20f95
jeanclad <jaanclad@naver.com>
2013-02-21 11:26:36 +0900

    re-upload

---
+++ Venus_001/VenusAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VenusAppDelegate.h
+//  Venus_001
+//
+//  Created by 권 회경 on 13. 2. 19..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenusAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Venus_001/VenusViewController.h
@@ -0,0 +1,15 @@
+//
+//  VenusViewController.h
+//  Venus_001
+//
+//  Created by 권 회경 on 13. 2. 19..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenusViewController : UIViewController <UIActionSheetDelegate,UIImagePickerControllerDelegate>
+
+- (IBAction)buttonPressed:(UIButton *)sender;
+
+@end
