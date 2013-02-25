131068c6caba62d833f76d1e82bdb560ad1d1094
kwon hoekyung <kwonhoekyung@kwon-ui-iMac.local>
2013-02-26 00:22:15 +0900

    add landscape mode in ios5

--- Venus_001/VenusFilmViewController.m
+++ Venus_001/VenusFilmViewController.m
@@ -15,8 +15,13 @@
 @synthesize assets;
 
 - (IBAction)buttonPressed:(UIBarButtonItem *)sender {
+/*
+    for(int i=0;i<[assets count];i++){
+        NSLog(@"뮤터블 : %@",[assets objectAtIndex:i]);
+    }
+ */
+
     [self dismissViewControllerAnimated:YES completion:nil];
-    NSLog(@"%@", getObjects:assets);
 }
 
 - (IBAction)cancel:(id)sender{
@@ -45,8 +50,14 @@
                       usingBlock:^(ALAssetsGroup *group, BOOL *stop){
                           [group enumerateAssetsUsingBlock:^(ALAsset *asset, NSUInteger index, BOOL *stop){
                               if (asset){
+                                  //ex 1
                                   NSLog(@"%@",asset);
-                                  [assets addObject:asset];
+                                  [assets addObject:[asset valueForProperty:ALAssetPropertyURLs]];
+                                  
+                                  //ex 2
+                                  NSLog(@"%d",index);
+                                  UIImage *image = [UIImage imageWithCGImage:asset.defaultRepresentation.fullScreenImage];
+                                  [self performSelectorOnMainThread:@selector(usePhotolibraryimage:) withObject:image waitUntilDone:NO];
                               }
                           }];
                       }failureBlock:^(NSError *error){
@@ -74,13 +85,32 @@
      */
 }
 
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+// IOS 6.0 이하 버전에서 landscape로 시작하지 않기 때문에 강제로 설정해주는 부분 (IOS 6.0 이상에서는 Call 되지 않음)
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation{
+    if ((toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) || (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft)){
+        return YES;
+    }
+    
+    return NO;
+}
+
 - (void)viewDidUnload {
     [super viewDidUnload];
 }
+
+#pragma ALAssetsLibrary selector
+- (void)usePhotolibraryimage:(UIImage *)myImage{
+    //Do your all UI related and all stuff here
+    NSLog(@"assets = %@", assets);
+}
 @end
+
+
+
--- Venus_001/VenusViewController.m
+++ Venus_001/VenusViewController.m
@@ -36,6 +36,15 @@
     [actionsheet showInView:self.view];
 }
 
+// IOS 6.0 이하 버전에서 landscape로 시작하지 않기 때문에 강제로 설정해주는 부분 (IOS 6.0 이상에서는 Call 되지 않음)
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation{
+    if ((toInterfaceOrientation == UIInterfaceOrientationLandscapeRight) || (toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft)){
+        return YES;
+    }
+    
+    return NO;
+}
+
 #pragma mark UIActionSheet Delegate
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
     UIImagePickerController *imagepickerController = [[UIImagePickerController alloc]init];
