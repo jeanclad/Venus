e8f1ed6d2a723aca2a50354cd54fe4e8fc10e445
jeanclad <jaanclad@naver.com>
2013-02-28 14:02:38 +0900

    110 source upload

---
+++ PhotoFarm/PhotoFarm/VenusAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  VenusAppDelegate.h
+//  PhotoFarm
+//
+//  Created by 권 회경 on 13. 2. 28..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class VenusViewController;
+
+@interface VenusAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) VenusViewController *viewController;
+
+@end
---
+++ PhotoFarm/PhotoFarm/VenusViewController.m
@@ -0,0 +1,29 @@
+//
+//  VenusViewController.m
+//  PhotoFarm
+//
+//  Created by 권 회경 on 13. 2. 28..
+//  Copyright (c) 2013년 Max. All rights reserved.
+//
+
+#import "VenusViewController.h"
+
+@interface VenusViewController ()
+
+@end
+
+@implementation VenusViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
